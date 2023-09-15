4b8b8a1ec29e5b13d07851b8e890133da5b07506
Bartłomoiej Milewski <bartlomiej.milewski@kainos.com>
2023-09-15 09:07:55 +0200

    Sales employee functions and views done

---
+++ SlodziakiFrontEnd/model/slSalesEmployee.ts
@@ -0,0 +1,8 @@
+export class SalesEmployee {
+    Id?: number
+    name?: string
+    salary?: number
+    bankAccountNumber?: string
+    nationalInsuranceNumber?: string
+    commissionRate?: number
+}
